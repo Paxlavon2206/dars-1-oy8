03a2cb535fec3d7ac756d223e50d46b5a1de86c2
Paxlavon2206 <paxlavon2206@gmail.com>
2024-04-05 07:16:10 +0500

    dar

--- main.js
+++ main.js
@@ -1,23 +1,104 @@
 "use strict";
-let obj = {
-    id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
-    address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: 929983874,
-        geo: {
-            lat: true,
-            lng: "81.1496",
+// interface User {
+//     id: number,
+//     name: string,
+//     username: string,
+//     email: string,
+//     address: {
+//         street: string,
+//         suite: string,
+//         city: string,
+//         zipcode: string,
+//         geo: {
+//             lat: string,
+//             lng: string
+//         }
+//     },
+//     phone: string,
+//     website: string,
+//     company: {
+//         name: string,
+//         catchPhrase: string,
+//         bs: string
+//     }
+// }
+const arr2 = [
+    {
+        "id": 1,
+        "age": 22,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
         },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
     },
-};
-function x(user) {
-    let a = obj.email;
-    return a;
+    {
+        "id": 2,
+        "age": 21,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+    {
+        "id": 3,
+        "age": 32,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157",
+            "geo": {
+                "lat": "-68.6102",
+                "lng": "-47.0653"
+            }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+            "name": "Romaguera-Jacobson",
+            "catchPhrase": "Face to face bifurcated interface",
+            "bs": "e-enable strategic applications"
+        }
+    },
+];
+// 2-Misol
+function namesAndAges(users) {
+    return users.map(({ name, age }) => ({ name, age }));
 }
-x(obj);
-console.log(x(obj));
+const namesAndAgesArray = namesAndAges(arr2);
+console.log(namesAndAgesArray);
--- main.ts
+++ main.ts
@@ -1,44 +1,213 @@
+// interface User {
+//     id: number,
+//     name: string,
+//     username: string,
+//     email: string,
+//     address: {
+//         street: string,
+//         suite: string,
+//         city: string,
+//         zipcode: string,
+//         geo: {
+//             lat: string,
+//             lng: string
+//         }
+//     },
+//     phone: string,
+//     website: string,
+//     company: {
+//         name: string,
+//         catchPhrase: string,
+//         bs: string
+//     }
+// }
 
+// const arr: User[] = [
+//     {
+//         "id": 1,
+//         "name": "Leanne Graham",
+//         "username": "Bret",
+//         "email": "Sincere@april.biz",
+//         "address": {
+//         "street": "Kulas Light",
+//         "suite": "Apt. 556",
+//         "city": "Gwenborough",
+//         "zipcode": "92998-3874",
+//         "geo": {
+//         "lat": "-37.3159",
+//         "lng": "81.1496"
+//         }
+//         },
+//         "phone": "1-770-736-8031 x56442",
+//         "website": "hildegard.org",
+//         "company": {
+//         "name": "Romaguera-Crona",
+//         "catchPhrase": "Multi-layered client-server neural-net",
+//         "bs": "harness real-time e-markets"
+//         }
+//         },
+//         {
+//         "id": 2,
+//         "name": "Ervin Howell",
+//         "username": "Antonette",
+//         "email": "Shanna@melissa.tv",
+//         "address": {
+//         "street": "Victor Plains",
+//         "suite": "Suite 879",
+//         "city": "Wisokyburgh",
+//         "zipcode": "90566-7771",
+//         "geo": {
+//         "lat": "-43.9509",
+//         "lng": "-34.4618"
+//         }
+//         },
+//         "phone": "010-692-6593 x09125",
+//         "website": "anastasia.net",
+//         "company": {
+//         "name": "Deckow-Crist",
+//         "catchPhrase": "Proactive didactic contingency",
+//         "bs": "synergize scalable supply-chains"
+//         }
+//         },
+//         {
+//         "id": 3,
+//         "name": "Clementine Bauch",
+//         "username": "Samantha",
+//         "email": "Nathan@yesenia.net",
+//         "address": {
+//         "street": "Douglas Extension",
+//         "suite": "Suite 847",
+//         "city": "McKenziehaven",
+//         "zipcode": "59590-4157",
+//         "geo": {
+//         "lat": "-68.6102",
+//         "lng": "-47.0653"
+//         }
+//         },
+//         "phone": "1-463-123-4447",
+//         "website": "ramiro.info",
+//         "company": {
+//         "name": "Romaguera-Jacobson",
+//         "catchPhrase": "Face to face bifurcated interface",
+//         "bs": "e-enable strategic applications"
+//         }
+//         },
+// ];
+// 1-Misol
+// function findAddressById(user: User): User['address'] {
+//     return user.address;
+// }
 
+// const userToFind = arr.find(user => user.id === 3);
+// if (userToFind) {
+//     const userAddress = findAddressById(userToFind);
+//     console.log(`Address for user with ID ${userToFind.id}:`, userAddress);
+// } else {
+//     console.log(`User not found.`);
+// }
 
-
-let obj: {
-    id: number,
-    name: string,
-    username: string,
-    email: string,
-    address:{
-        street:string,
-        suite: string,
-        city: string,
-        zipcode: number,
-        geo: {
-          lat: boolean,
-          lng: string,  
-        },
-    }
-} = {
-  id: 1,
-  name: "Leanne Graham",
-  username: "Bret",
-  email: "Sincere@april.biz",
+interface User {
+  id: number;
+  name: string;
+  age: number;
+  username: string;
+  email: string;
   address: {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: 929983874,
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
     geo: {
-      lat: true,
-      lng: "81.1496",
-    },
-  },
-};
-
-function x (user: object){
- let a =  obj.email
-return a
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
+const arr2: User[] = [
+    {
+        "id": 1,
+        "age": 22,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+        }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+        }
+        },
+        {
+        "id": 2,
+        "age": 21,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+        }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+        }
+        },
+        {
+        "id": 3,
+        "age": 32,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+        "lat": "-68.6102",
+        "lng": "-47.0653"
+        }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+        }
+        },
+];
+// 2-Misol
 
-x(obj)
-
-console.log(x(obj));
+function namesAndAges(users: User[]): { name: string; age: number }[] {
+    return users.map(({ name, age }) => ({ name, age }));
+  }
+  const namesAndAgesArray = namesAndAges(arr2);
+console.log(namesAndAgesArray);
